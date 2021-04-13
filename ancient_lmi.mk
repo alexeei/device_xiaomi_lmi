83dc2df22ab11ef0dedc80e3ebe23ee3227ab420
soumyo19 <fsoummya@gmail.com>
2021-04-13 04:04:31 +0000

    lmi: bringup Ancient :D

--- ancient_lmi.mk
+++ ancient_lmi.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lmi
+PRODUCT_NAME := ancient_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
