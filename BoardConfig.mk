9219445895619a9b9e178e03d664b4e3e4fb9510
SebaUbuntu <barezzisebastiano@gmail.com>
2021-04-25 07:17:26 +0200

    lmi: Move common device.mk inclusion to the bottom
    Change-Id: I608e4442875fb46a2cf816863302c10db27276de

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,9 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from xiaomi sm8250-common
-include device/xiaomi/sm8250-common/BoardConfigCommon.mk
-
 DEVICE_PATH := device/xiaomi/lmi
 
 # Bluetooth
@@ -40,6 +37,9 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
 
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+
+# Inherit from xiaomi sm8250-common
+include device/xiaomi/sm8250-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
 include vendor/xiaomi/lmi/BoardConfigVendor.mk
